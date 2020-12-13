074105dcb3bee1a8601240f3c0ea267249aa648e
Tigger Wong <tigger@appit.com.hk>
2020-12-13 17:11:49 +0800

    feat: Update preview for nwd50

---
+++ precache-manifest.28c020b0cdcbd3d528483b31ce10f2e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5069290e36c134f2958f73df8dba7ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e383352cea2508096a1",
+    "url": "/static/css/main.a4e8a829.chunk.css"
+  },
+  {
+    "revision": "ecd560b0b1dc3994a109",
+    "url": "/static/js/2.47236bd1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.47236bd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e383352cea2508096a1",
+    "url": "/static/js/main.350fe65b.chunk.js"
+  },
+  {
+    "revision": "d13e4bc4c7e8b0344ab1",
+    "url": "/static/js/runtime-main.a24f6be9.js"
+  }
+]);
