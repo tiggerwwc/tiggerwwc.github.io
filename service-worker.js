e1d6a1e0162eaa1b5675745c69b09d44501d6afe
Tigger Wong <tigger@appit.com.hk>
2020-12-13 23:04:46 +0800

    feat: Add basic rwd to nwd50

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28c020b0cdcbd3d528483b31ce10f2e2.js"
+  "/precache-manifest.eae1c2ec3c096217b25d8037221ce26c.js"
 );
 
 self.addEventListener('message', (event) => {
