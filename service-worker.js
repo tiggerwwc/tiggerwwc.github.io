1fc65d2980751dee9664d28fa5b529fda6a31ad2
Tigger Wong <tigger@appit.com.hk>
2020-12-21 00:50:21 +0800

    feat: Update for second half sections

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdbb55dc922ad0e5122394b8932fe6c7.js"
+  "/precache-manifest.5444a69bb22c24461a916da771b03125.js"
 );
 
 self.addEventListener('message', (event) => {
