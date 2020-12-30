5c8bfbdebddd7ec5f6ef42048d36acc5f359be17
Tigger Wong <tigger@appit.com.hk>
2020-12-31 00:29:20 +0800

    feat: Update for Dec 30th comments

---
+++ precache-manifest.00796df5c8b075d4895cb1c9fe0a3fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4508f73a1185c1b0697c76e06ad464e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d852b6395101d3ace02",
+    "url": "/static/css/main.b826fa67.chunk.css"
+  },
+  {
+    "revision": "2d4ec60ed3d3a093b794",
+    "url": "/static/js/2.6d4d5871.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6d4d5871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d852b6395101d3ace02",
+    "url": "/static/js/main.5f0a76bf.chunk.js"
+  },
+  {
+    "revision": "d13e4bc4c7e8b0344ab1",
+    "url": "/static/js/runtime-main.a24f6be9.js"
+  }
+]);
