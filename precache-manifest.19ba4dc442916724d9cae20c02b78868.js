c27f99749a552dba2cfd811a5c4a4e03ea16e299
Tigger Wong <tigger@appit.com.hk>
2020-12-31 01:10:34 +0800

    feat: Update for some minor adjustment

---
+++ precache-manifest.19ba4dc442916724d9cae20c02b78868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7737909babc241eab0178024da99fc96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "090716887884796624c1",
+    "url": "/static/css/main.ab3649b1.chunk.css"
+  },
+  {
+    "revision": "2d4ec60ed3d3a093b794",
+    "url": "/static/js/2.6d4d5871.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6d4d5871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "090716887884796624c1",
+    "url": "/static/js/main.5f0a76bf.chunk.js"
+  },
+  {
+    "revision": "d13e4bc4c7e8b0344ab1",
+    "url": "/static/js/runtime-main.a24f6be9.js"
+  }
+]);
