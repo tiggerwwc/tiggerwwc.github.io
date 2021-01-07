39c6f664a708006d87e858af2f27e05c016b8eea
Tigger Wong <tigger@appit.com.hk>
2021-01-07 13:53:58 +0800

    fix: Fix safari control

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cf9dba513fac835648289378d127619.js"
+  "/precache-manifest.3f95dbf7fa5f2c2fff784e7f3afcdc80.js"
 );
 
 self.addEventListener('message', (event) => {
