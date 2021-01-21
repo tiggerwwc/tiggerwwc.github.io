64b502464264e6f566b5e9e81d6efc558e80cf2d
Tigger Wong <tigger@appit.com.hk>
2021-01-21 21:31:03 +0800

    fix: Fix menu style

---
+++ precache-manifest.b592548d684b7dce6db903347685cccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0178639641352da3b78fbbe3dd72c4ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2bffb9ade5d3aea0e7",
+    "url": "/static/css/main.3ba8a1ec.chunk.css"
+  },
+  {
+    "revision": "0e4e30734425901ef4cb",
+    "url": "/static/js/2.a34b373c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a34b373c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce2bffb9ade5d3aea0e7",
+    "url": "/static/js/main.41c2b5f0.chunk.js"
+  },
+  {
+    "revision": "d13e4bc4c7e8b0344ab1",
+    "url": "/static/js/runtime-main.a24f6be9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7c63206dc92409b92cc6dd8641e63a9.js"
+  "/precache-manifest.b592548d684b7dce6db903347685cccc.js"
 );
 
 self.addEventListener('message', (event) => {
