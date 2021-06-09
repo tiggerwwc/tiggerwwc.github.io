a765e7f96228ce8346b8ec68a07f4b03aa365a8c
Tigger Wong <tigger@appit.com.hk>
2021-06-09 20:33:47 +0800

    feat: Add key and remove console

---
+++ precache-manifest.7d12aa48c78a0be93496cab584ecc069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ab5bdff11d50b9969cb20d968c59da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16faada294d36f9c750",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "cabb2e02a93a84d2dca4",
+    "url": "/static/css/main.010e86c7.chunk.css"
+  },
+  {
+    "revision": "f16faada294d36f9c750",
+    "url": "/static/js/2.14f2b9e3.chunk.js"
+  },
+  {
+    "revision": "3cf87206dbcb278024654205dc88a75e",
+    "url": "/static/js/2.14f2b9e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cabb2e02a93a84d2dca4",
+    "url": "/static/js/main.1fac19a9.chunk.js"
+  },
+  {
+    "revision": "2b66ad868130f521470c",
+    "url": "/static/js/runtime-main.0ec482cf.js"
+  }
+]);
