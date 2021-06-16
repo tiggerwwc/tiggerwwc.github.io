975ea4c3d8861c351f0c502bb7195e129d0b9c48
Tigger Wong <tigger@appit.com.hk>
2021-06-17 01:24:09 +0800

    feat: Remove extra paragraph

---
+++ precache-manifest.2579e3ec3cc725483cbe9abce695aab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6494789d359f69e345d1c6b75cb57d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16faada294d36f9c750",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "70a39505d14576643728",
+    "url": "/static/css/main.010e86c7.chunk.css"
+  },
+  {
+    "revision": "f16faada294d36f9c750",
+    "url": "/static/js/2.14f2b9e3.chunk.js"
+  },
+  {
+    "revision": "3cf87206dbcb278024654205dc88a75e",
+    "url": "/static/js/2.14f2b9e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70a39505d14576643728",
+    "url": "/static/js/main.f05f9ba0.chunk.js"
+  },
+  {
+    "revision": "2b66ad868130f521470c",
+    "url": "/static/js/runtime-main.0ec482cf.js"
+  }
+]);
